3472c6c7e322a8994cf384f56a30e7b30d5ec69d
korArty2488 <48129496+KorArty24@users.noreply.github.com>
2019-11-15 12:46:55 +0300

    End of Section 3

---
+++ DTOs/UserForRegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.DTOs
+{
+    public class UserForRegisterDTO
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8,MinimumLength =4,ErrorMessage ="Your password must be between 4 and 8 symbols")]
+        public string Password { get; set; }
+    }
+}
